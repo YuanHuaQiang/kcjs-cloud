6992eb0e6578085d5beeda3c2414d13ddcad0edf
YuanHuaQiang <yuanhuaqiang.work@gmail.com>
2025-03-20 17:34:35 +0800

    b5ae1716

--- kcjs-cloud-dubbo-sp-provider/src/main/java/com/kcjs/cloud/sp/provider/KcjsCloudSpProviderApplication.java
+++ kcjs-cloud-dubbo-sp-provider/src/main/java/com/kcjs/cloud/sp/provider/KcjsCloudSpProviderApplication.java
@@ -3,6 +3,7 @@ package com.kcjs.cloud.sp.provider;
 import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 
